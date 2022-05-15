74e01216c3a258ff48c75ae987f68b5d7c82e884
kriziu <brunodzi07@gmail.com>
2022-05-15 17:29:50 +0200

    Added cloudinary provider to store images.

--- config/plugins.js
+++ config/plugins.js
@@ -16,4 +16,18 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
 });
